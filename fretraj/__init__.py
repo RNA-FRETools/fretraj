1162b1ac8b86e171db895d12b02eda6470ac7dfb
fdsteffen <35061965+fdsteffen@users.noreply.github.com>
2019-07-19 23:27:20 +0200

    Add entry point to PyMOL plugin

--- fretraj/__init__.py
+++ fretraj/__init__.py
@@ -2,3 +2,25 @@ from fretraj.cloud import *
 from fretraj.export import *
 from fretraj.fret import *
 from fretraj.fit import *
+from fretraj.isosurf import *
+from fretraj.gui import *
+
+dialog = None
+
+
+def __init_plugin__(app=None):
+    """
+    Add FRETraj plugin to the Plugins Menu
+    """
+    from pymol.plugins import addmenuitemqt
+    addmenuitemqt('FRETraj', run_plugin_gui)
+
+
+def run_plugin_gui():
+    """
+    Create the GUI Window
+    """
+    global dialog
+    if dialog is None:
+        dialog = App(_pymol_running=True)
+    dialog.show()
